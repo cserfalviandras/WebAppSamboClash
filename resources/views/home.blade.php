fde7b16a571a39673e4d27e191e8b8d8eaefc7aa
Cserfalvi_Andras <cserfalviandras@gmail.com>
2019-06-16 10:55:11 +0200

    Validate role in middleware

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-@role('admin')
-    I am admin!
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Home</div>
-                </div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Home</div>
             </div>
         </div>
     </div>
-@else
-    I am not an admin...
-@endrole
+</div>
 @endsection
--- storage/framework/views/36b487ac9d0fc66b97eea5b81f3ee5aae17b1109.php
+++ storage/framework/views/36b487ac9d0fc66b97eea5b81f3ee5aae17b1109.php
@@ -1,18 +1,13 @@
 <?php $__env->startSection('content'); ?>
-<?php if(auth()->check() && auth()->user()->hasRole('spectator')): ?>
-    I am admin!
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Home</div>
-                </div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Home</div>
             </div>
         </div>
     </div>
-<?php else: ?>
-    I am not an admin...
-<?php endif; ?>
+</div>
 <?php $__env->stopSection(); ?>
 
 <?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\Development\POC_laravel_sambo\WebAppSamboClash_versioned\resources\views/home.blade.php ENDPATH**/ ?>
