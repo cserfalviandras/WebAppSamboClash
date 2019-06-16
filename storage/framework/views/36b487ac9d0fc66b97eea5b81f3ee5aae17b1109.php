f124b96b0d565fea3f997ee7989a693797db4f06
Cserfalvi_Andras <cserfalviandras@gmail.com>
2019-06-16 10:49:01 +0200

    Assign persmission(s) to role(s) and assign role(s) to user(s) and validate certain role on one page

--- storage/framework/views/36b487ac9d0fc66b97eea5b81f3ee5aae17b1109.php
+++ storage/framework/views/36b487ac9d0fc66b97eea5b81f3ee5aae17b1109.php
@@ -1,13 +1,18 @@
 <?php $__env->startSection('content'); ?>
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Home</div>
+<?php if(auth()->check() && auth()->user()->hasRole('spectator')): ?>
+    I am admin!
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Home</div>
+                </div>
             </div>
         </div>
     </div>
-</div>
+<?php else: ?>
+    I am not an admin...
+<?php endif; ?>
 <?php $__env->stopSection(); ?>
 
 <?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\Development\POC_laravel_sambo\WebAppSamboClash_versioned\resources\views/home.blade.php ENDPATH**/ ?>
