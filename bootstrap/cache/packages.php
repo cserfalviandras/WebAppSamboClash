aff4d78661bdc5d63862d9fe37a793aad36a1fac
Cserfalvi_Andras <cserfalviandras@gmail.com>
2019-06-16 06:45:54 +0200

    Install spatie

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -34,4 +34,11 @@
       0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
     ),
   ),
+  'spatie/laravel-permission' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\Permission\\PermissionServiceProvider',
+    ),
+  ),
 );
