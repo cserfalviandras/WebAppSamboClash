a09ca9c05b62f30c1a84151c03d4431a30d9e369
Cserfalvi_Andras <cserfalviandras@gmail.com>
2019-06-24 06:57:51 +0200

    Implement store function for clash creation.

--- app/Http/Controllers/ClashesController.php
+++ app/Http/Controllers/ClashesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\clash;
 
 class ClashesController extends Controller
 {
@@ -19,6 +20,22 @@ class ClashesController extends Controller
 
     public function store()
     {
-        dd(request()->all());
+        $data = request()->validate([
+            'inputAgeGroup' => 'required',
+            'inputWeightCat' => 'required',
+            'inputStartTime' => 'required',
+        ]);
+
+        $clash = new clash();
+        $clash->age_group_id = request('inputAgeGroup');
+        $clash->weight_cat_id = request('inputWeightCat');
+        $clash->start_time = request('inputStartTime');
+        //$clash->scoreboard_id = NewScoreBoard();
+        $clash->scoreboard_id = 0;
+        $clash->winner_id = 0;   
+        $clash->clash_status_id = 0;
+        $clash->save();
+
+        return redirect('success');
     }
 }
