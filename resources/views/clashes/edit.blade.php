26cf5492401b7c94e00a8aa17479f0f6e4346f13
Cserfalvi_Andras <cserfalviandras@gmail.com>
2019-07-24 04:49:48 +0200

    Print datetime test.

--- resources/views/clashes/edit.blade.php
+++ resources/views/clashes/edit.blade.php
@@ -9,7 +9,9 @@
                 <div class="card-body">
                     <h5 class="card-title">Mérkőzés</h5>
                     <h6 class="card-subtitle mb-2 text-muted">Szerkesztés</h6>
-
+                    @php
+                        var_dump($clash->start_time);
+                    @endphp
                     <form class="pt-3" action="/clashes/update" enctype="multipart/form-data" method="post">
                         @csrf
                         <div class="form-group">
