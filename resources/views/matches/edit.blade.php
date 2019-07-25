15bd88496418ec3a04dfbde59fc500c0d89fe98b
Cserfalvi_Andras <cserfalviandras@gmail.com>
2019-07-25 05:22:10 +0200

    Create basic layout for match edit.

--- resources/views/matches/edit.blade.php
+++ resources/views/matches/edit.blade.php
@@ -6,8 +6,67 @@
         <div class="col">
             <div class="card">
                 <div class="card-body">
-                    <h5 class="card-title">Küzdelem</h5>
-                    <h6 class="card-subtitle mb-2 text-muted"></h6>
+                    <div class="row">
+                        <div class="col-sm">
+                            Küzdelem idő
+                        </div>
+                        <div class="col-sm">
+                            05:00
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-sm">
+                            
+                        </div>
+                        <div class="col-sm">
+                            
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-sm">
+                            <div class="card">
+                                <div class="card-body bg-danger text-white">
+                                    <h5 class="card-title">Ellenfél 1</h5>
+                                    <h6 class="card-subtitle mb-2 text-muted"></h6>
+                                    <div class="form-group">
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm">
+                            <div class="card">
+                                <div class="card-body bg-primary text-white">
+                                    <h5 class="card-title">Ellenfél 2</h5>
+                                    <h6 class="card-subtitle mb-2 text-muted"></h6>
+                                    <div class="form-group">
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="mt-3 row">  
+                        <div class="col-sm">
+                            <div class="row">
+                                <div class="col-sm">
+                                    <button type="" class="btn btn-secondary">Indít</button>
+                                </div>
+                                <div class="col-sm">
+                                    <button type="" class="btn btn-secondary">Állj</button>
+                                </div>
+                                <div class="col-sm">
+                                    <button type="" class="btn btn-danger">Visszaállít</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm">
+                            <button type="" class="btn btn-secondary btn-lg">Mérkőzés vége</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
