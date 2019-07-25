a3fced60eef79a9e785cf9b7ea012e320111df81
Cserfalvi_Andras <cserfalviandras@gmail.com>
2019-07-25 21:05:40 +0200

    Expand match component to the entire match card.

--- resources/views/components/match_panel.blade.php
+++ resources/views/components/match_panel.blade.php
@@ -1,37 +1,49 @@
-<table class="table">
-    <tr>
-        <td>
-            <button type="" class="btn btn-dark  btn-block">-</button>
-        </td>
-        <td>
-            <button type="" class="btn btn-dark  btn-block">-</button>
-        </td>
-    </tr>
+@php
+    use App\competitor;
+    $competitor_name = competitor::where('id', ($comp_id+1))->first()->name;
+@endphp
+<div class="card">
+    <div class="card-body text-white <?php if($dress_id == 1){echo 'bg-primary'; } else { echo 'bg-danger'; } ?>">
+        <h5 class="card-title">{{$competitor_name}}</h5>
+        <h6 class="card-subtitle mb-2 text-muted"></h6>
+        <div class="form-group">
+        <table class="table">
+            <tr>
+                <td>
+                    <button type="" class="btn btn-dark  btn-block">-</button>
+                </td>
+                <td>
+                    <button type="" class="btn btn-dark  btn-block">-</button>
+                </td>
+            </tr>
 
-    <tr>
-        <td>
-            <button type="" class="btn btn-dark  btn-block">-</button>
-        </td>
-        <td>
-            <button type="" class="btn btn-dark  btn-block">-</button>
-        </td>
-    </tr>
+            <tr>
+                <td>
+                    <button type="" class="btn btn-dark  btn-block">-</button>
+                </td>
+                <td>
+                    <button type="" class="btn btn-dark  btn-block">-</button>
+                </td>
+            </tr>
 
-    <tr>
-        <td>
-            <button type="" class="btn btn-dark  btn-block">-</button>
-        </td>
-        <td>
-            <button type="" class="btn btn-dark  btn-block">-</button>
-        </td>
-    </tr>
+            <tr>
+                <td>
+                    <button type="" class="btn btn-dark  btn-block">-</button>
+                </td>
+                <td>
+                    <button type="" class="btn btn-dark  btn-block">-</button>
+                </td>
+            </tr>
 
-    <tr>
-        <td>
-            <button type="" class="btn btn-dark  btn-block">-</button>
-        </td>
-        <td>
-            <button type="" class="btn btn-dark  btn-block">-</button>
-        </td>
-    </tr>
-</table>
+            <tr>
+                <td>
+                    <button type="" class="btn btn-dark  btn-block">-</button>
+                </td>
+                <td>
+                    <button type="" class="btn btn-dark  btn-block">-</button>
+                </td>
+            </tr>
+        </table>
+        </div>
+    </div>
+</div>
--- resources/views/matches/edit.blade.php
+++ resources/views/matches/edit.blade.php
@@ -26,34 +26,18 @@
 
                     <div class="row">
                         <div class="col-sm">
-                            <div class="card">
-                                <div class="card-body bg-danger text-white">
-                                    <h5 class="card-title">Ellenfél 1</h5>
-                                    <h6 class="card-subtitle mb-2 text-muted"></h6>
-                                    <div class="form-group">
-                                        @include('components.match_panel', [
-                                            'clash_id' => $clash->id,
-                                            'comp_id' => $clashCompetitors->comp_id,
-                                            'dress_id' => $clashCompetitors->dress_id
-                                            ])
-                                    </div>
-                                </div>
-                            </div>
+                            @include('components.match_panel', [
+                                'clash_id' => $clash->id,
+                                'comp_id' => $clashCompetitors->comp_id,
+                                'dress_id' => $clashCompetitors->dress_id
+                                ])
                         </div>
                         <div class="col-sm">
-                            <div class="card">
-                                <div class="card-body bg-primary text-white">
-                                     <h5 class="card-title">Ellenfél 2</h5> {{-- {{ App\competitor::where('id', $clashCompetitors->comp_id_2)->first()->name }} --}}
-                                    <h6 class="card-subtitle mb-2 text-muted"></h6>
-                                    <div class="form-group">
-                                        @include('components.match_panel', [
-                                            'clash_id' => $clash->id,
-                                            'comp_id' => $clashCompetitors->comp_id_2,
-                                            'dress_id' => $clashCompetitors->dress_id_2
-                                            ])
-                                    </div>
-                                </div>
-                            </div>
+                            @include('components.match_panel', [
+                                'clash_id' => $clash->id,
+                                'comp_id' => $clashCompetitors->comp_id_2,
+                                'dress_id' => $clashCompetitors->dress_id_2
+                                ])
                         </div>
                     </div>
 
