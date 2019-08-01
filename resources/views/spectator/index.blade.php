507f94fd9958c3a51a2bbdd41dcdebfc8958234e
Cserfalvi_Andras <cserfalviandras@gmail.com>
2019-08-01 04:27:07 +0200

    Create basic layout.

--- resources/views/spectator/index.blade.php
+++ resources/views/spectator/index.blade.php
@@ -1,1 +1,17 @@
-Nézői oldal
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <div class="my-5 row">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
