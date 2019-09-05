5e5028debb7c2afb4f661e7c8978cade02790e54
Cserfalvi_Andras <cserfalviandras@gmail.com>
2019-09-05 17:51:01 +0200

    Fix incorrect organization name query on match panel.

--- resources/views/components/match_panel.blade.php
+++ resources/views/components/match_panel.blade.php
@@ -1,6 +1,6 @@
 @php
     use App\competitor;
-    $competitor_name = competitor::where('id', ($comp_id+1))->first()->name;
+    $competitor_name = competitor::where('id', ($comp_id))->first()->name;
 @endphp
 <div class="card">
     <div class="card-body text-white <?php if($dress_id == 1){echo 'bg-primary'; } else { echo 'bg-danger'; } ?>">
